c62dc1c7cc78c52fe48dd95c71fc0a1e83a7460d
krishika <singh.kris@northeastern.edu>
2021-09-26 21:15:24 -0400

    assignment 1 submission

---
+++ Lab1_Assignnment1/src/model/Person.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Krish
+ */
+public class Person {
+    
+    private String name;
+    private String addressLine1 ;
+    private String addressLine2;
+    private String Pincode;
+    private String Mobile;
+    private String faxNumber;
+    private String emailID;
+    private String socialSecurityNumber;
+    private String medicalRecordNumber;
+    private String healthPlanBenificiaryNumber;
+    private String accountNumber;
+    private String licenseNumber;
+    private String licensePlateNumber;
+    private String vehicleIdentifier;
+    private String deviceIdentifier;
+    private String linkedinLink;
+    private String ipAddress;
+    private String imageUpload;
+    private String uIN;
+    private String dOB;
+
+    
+    public String getDob() {
+        return dOB;
+    }
+
+    public void setDob(String dOB) {
+        this.dOB = dOB;
+    }
+
+    
+   public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getPincode() {
+        return Pincode;
+    }
+
+    public void setPincode(String Pincode) {
+        this.Pincode = Pincode;
+    }
+
+    public String getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(String Mobile) {
+        this.Mobile = Mobile;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getHealthPlanBenificiaryNumber() {
+        return healthPlanBenificiaryNumber;
+    }
+
+    public void setHealthPlanBenificiaryNumber(String healthPlanBenificiaryNumber) {
+        this.healthPlanBenificiaryNumber = healthPlanBenificiaryNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getLinkedinLink() {
+        return linkedinLink;
+    }
+
+    public void setLinkedinLink(String linkedinLink) {
+        this.linkedinLink = linkedinLink;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getImageUpload() {
+        return imageUpload;
+    }
+
+    public void setImageUpload(String imageUpload) {
+        this.imageUpload = imageUpload;
+    }
+
+    public String getuIN() {
+        return uIN;
+    }
+
+    public void setuIN(String uIN) {
+        this.uIN = uIN;
+    }
+    
+    
+    
+    
+}
